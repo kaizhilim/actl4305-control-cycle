ab5daa18b3faf972238224f172ea9c71c2804708
KaiZhiLim <kaizhilim@gmail.com>
2022-09-26 15:45:56 +1000

    R Project Set Up

---
+++ 00 source.R
@@ -0,0 +1,8 @@
+# Packages
+if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
+pacman::p_load(
+  tidyverse
+)
+
+
+
---
+++ 01 Data Cleaning/01 Import.R
@@ -0,0 +1,3 @@
+source("00 source.R")
+
+ass_data = read_csv("Assignment Data/Assignment Data.csv")
