92e9c81e92a2e83b7db442dc1dedc3981dfca5aa
KaiZhiLim <kaizhilim@gmail.com>
2022-09-27 19:14:11 +1000

    Added policy_df & created occupation_risk column

--- 00 source.R
+++ 00 source.R
@@ -1,8 +1,6 @@
 # Packages
 if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
 pacman::p_load(
-  tidyverse
+  tidyverse,
+  lubridate
 )
-
-# Datasets
-load("00 envr/Compulsory/ass_copy.R")
--- 01 Data Cleaning/Data Cleaning Interface.R
+++ 01 Data Cleaning/Data Cleaning Interface.R
@@ -22,11 +22,48 @@ ass_copy <- ass_data%>%
   
   # Remove policies with no property cover
   filter(property_cover)%>%
+  
+  # Simplify occupation
+  mutate(occupation_risk = factor(str_sub(occupation, 1, 1)))%>%
+  
+  # Remove redundant columns
   select(-property_cover)
 
-## 2. NA Cleaning ####
+## 2. Policyholder Details ####
+policyholder_cols = c(
+  # Policy Identifier
+  "policyno", "situation_num",
+  
+  # Packaged policy
+  "LossofIncome_cover",
+  
+  # Policy Dates
+  "effectdate", "expirydate", "ym", "xm",
+  
+  # Building address
+  "riskpostcode", "state",
+  
+  # Building details
+  "building_age", "building_type", "construction_walls", "construction_floor",
+  "sprinkler_type",
+  
+  # Occupation
+  "occupation_risk"
+)
 
-# KELLY WRITE CODE HERE
+policy_df <- ass_copy%>%
+  select(all_of(policyholder_cols))%>%
+  relocate(all_of(policyholder_cols))%>%
+  arrange(policyno, situation_num, effectdate, expirydate,
+          desc(ym), desc(xm))%>%
+  distinct(across(!c(ym, xm)), .keep_all = T)%>%
+  group_by(policyno, situation_num)%>%
+  mutate(policy_version = dense_rank(paste(effectdate, expirydate)))%>%
+  ungroup()%>%
+  mutate(cancelled = expirydate - as.Date(paste0(ym,"-", xm, "-01")) > 61)
 
 # Save as R File
 save(ass_copy, file = "00 envr/Compulsory/ass_copy.R")
+save(policy_df, file = "00 envr/Compulsory/policy_df.R")
+
+rm(policyholder_cols)
