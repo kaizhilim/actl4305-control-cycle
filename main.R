256a8e290d36cb8642602bb14aadee6e507779a2
KaiZhiLim <kaizhilim@gmail.com>
2022-11-04 21:45:16 +1100

    Saving policy_claims.Rda

--- main.R
+++ main.R
@@ -1,26 +1,28 @@
-source("01 Data Cleaning/Data Cleaning Interface.R")
-source("03 Preparation/01 Data Standardization Interface.R")
-source("03 Preparation/02 Geo Code Interface.R")
-
-## 1. Data Cleaning ####
-ass_data = read_csv("Assignment Data/Assignment Data.csv")
-ass_clean_na = data_cleaning_interface(ass_data)
-# save(ass_clean_na, file = "00 envr/Cleaning/ass_clean_na.Rda")
-rm(ass_data)
-
-## 2. Data Standardisation ####
-# Environment Management
+## 00. Environment Management ####
 init_dir = ls()
 
-policy_claims<- data_standardization_interface(ass_clean_na)
+reload_ass_data = FALSE
 
-rm(list = setdiff(ls(), c(init_dir, 'policy_claims')))
-rm(init_dir)
-
-save(policy_claims, file = "00 envr/Compulsory/policy_claims.Rda")
+if(reload_ass_data) {
+  source("01 Data Cleaning/Data Cleaning Interface.R")
+  source("03 Preparation/01 Data Standardization Interface.R")
+  
+  ## 1. Data Cleaning ####
+  ass_data = read_csv("Assignment Data/Assignment Data.csv")
+  ass_clean_na = data_cleaning_interface(ass_data)
+  # save(ass_clean_na, file = "00 envr/Cleaning/ass_clean_na.Rda")
+  rm(ass_data)
+  
+  ## 2. Data Standardisation ####
+  policy_claims<- data_standardization_interface(ass_clean_na)
+  
+  save(policy_claims, file = "00 envr/Compulsory/policy_claims.Rda")
+} else {
+  load("00 envr/Compulsory/policy_claims.Rda")
+}
 
 ## 3. Geo Code Preparation ####
-# load("00 envr/Compulsory/policy_claims.Rda")
+source("03 Preparation/02 Geo Code Interface.R")
 geo_code_grid = 
   expand_grid(
     threshold_riskpostcode = seq(10, 50, 5),
@@ -33,4 +35,8 @@ geo_code_grid =
     threshold_sa4name = threshold_sa3name * sa4name_multiplier
   )
 
-policy_claims_grid = geo_code_interface(geo_code)
+policy_claims_grid = geo_code_interface(geo_code_grid, 
+                                        policy_claims = policy_claims)
+
+
+rm(list = setdiff(ls(), c(init_dir, 'policy_claims', 'policy_claims_grid')))
